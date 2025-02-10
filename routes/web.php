76e8322b7f7cfb880309caaf6b33cbc1fd08b3ae
AzizbekZiyodullayev2024 <ziyodulayeva9@gmail.com>
2025-02-10 21:24:14 +0500

    Initial

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('');
 });
-//Azizbek
+Route::get('/test', function () {
+    return view('test');
+});
